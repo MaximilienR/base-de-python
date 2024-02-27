de4d1daec8a3d3e8646cda216f761f76a4f24649
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2024-02-27 17:11:38 +0100

    base python texte,convertion,splint,

--- cours.py
+++ cours.py
@@ -7,11 +7,21 @@ c,d=45,46
 e=f=g=50;
 #afficher le texte+concacténation
 print ('bonjour '+ ' '+ identity);
+"bonjour".replace("jour","soir")
 #utiliser les chiffre
 grandFrere =int(2)
 petitFrere = int(5)
 ageTT= grandFrere+petitFrere
 print(ageTT)
 
- 
-    
+h=3
+i="2"
+#convertir str en int 
+i=int(i) 
+print(h+i)
+#split
+liste="1,2,3,4,5".split(",")
+print(liste);
+#compter les caratectre 
+phrase ="combien y a t il de mot dans ma phrase "
+phrase.count("&")
---
+++ ex1.py
@@ -0,0 +1,9 @@
+ 
+ 
+a=str(10)+ ' ' +'+'+'5'+' '+'égal 15'#10 +5 est égal à 15 
+b=10+int('5')#15 OK
+c=("L'addition de 10+5 est égal à ")+ str(10+5) #l'addition de 10 + 5 est égal à 15 
+
+print(a)
+print(b)
+print(c)
