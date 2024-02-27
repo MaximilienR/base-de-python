3c6d53a679635d0592cf7b69a70532abfadc2201
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2024-02-27 12:48:51 +0100

    first commit

---
+++ random.py
@@ -0,0 +1,17 @@
+  #les bases de python 
+
+#les variable + faire un demande utilisateur 
+identity = input("veuillez saisir votre nom"+ " " );
+#affectation multiple 
+c,d=45,46
+e=f=g=50;
+#afficher le texte+concacténation
+print ('bonjour '+ ' '+ identity);
+#utiliser les chiffre
+grandFrere =int(2)
+petitFrere = int(5)
+ageTT= grandFrere+petitFrere
+print(ageTT)
+
+ 
+    
