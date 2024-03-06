6e593397fd4ac7427e2ea320eb73cabb5e504a0d
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2024-03-06 11:02:48 +0100

    clear code password program

--- password.py
+++ password.py
@@ -3,6 +3,12 @@ password=str(input("veuillez choisir un mot de passe de d'au moins 10 caracter a
 
 if len(password)<8:
     print("veuilllez choisir un mdp plus long")
+    
+        #Vérifiez si tous les caractères du texte sont des chiffres :
+elif password.isdigit():
+    print("votre mot de passe ne contient que des chiffres")
+elif password.isalpha():
+    print("votre mot de passe ne contient  que des lettres")
 
 else:
     print("inscription reussit") 
