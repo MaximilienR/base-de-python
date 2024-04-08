420c764010abe0a09e42861191e7b7cfc8d6d976
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2024-04-08 16:59:02 +0200

    add test

--- django-web-app/projet_test/tuto/manga/tests.py
+++ django-web-app/projet_test/tuto/manga/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Author
 # Create your tests here.
+class AuthorTestCase(TestCase):
+    def setUp(self):
+        self.author=Author.objects.create(name="Random Smith")
+        
+    def test_is_correct_instance(self):
+        self.assertIsInstance(self.author,Author)
+    
+    def test_existe(self):
+        author =Author.objects.get(pk=1)
+        self.assertFalse(author)
